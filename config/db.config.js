6b4d31f6efb2f42eeb9b2aa49edb9107501cb0da
Julián David Valencia <julianv302@gmail.com>
2025-05-07 02:29:04 -0500

    fix: fix password name

--- config/db.config.js
+++ config/db.config.js
@@ -9,7 +9,7 @@ export const sequelize = new Sequelize({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   dialect: 'postgres',
   logging: false,
 });
